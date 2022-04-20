7d8d3c1ae1cb11bac7a2dac70e18e65872128310
Abanob Tawfik <goonpizza@hotmail.com>
2022-04-20 23:50:01 +1000

    rewrote directory structure and spec additions

--- src/globals/global_variables.ts
+++ src/globals/global_variables.ts
@@ -2,7 +2,6 @@ const global_variables = {
   database_path: 'database.json',
   tmp_database_path: 'database_tmp.json',
   dictionary_name: 'dictionary',
-  translation_name: 'translation'
 }
 
 export default global_variables
