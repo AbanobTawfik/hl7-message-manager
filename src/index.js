ec99c9a6a61e70aba12a8415912bf1df56b111af
Abanob Tawfik <goonpizza@hotmail.com>
2022-07-18 09:31:34 +1000

    creating dictionary module basis

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import Appt from './dict_test.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Appt />
   </React.StrictMode>
 );
 
