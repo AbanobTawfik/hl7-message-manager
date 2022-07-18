49657738d182fc7a61df26994982ce321c8080e9
Abanob Tawfik <goonpizza@hotmail.com>
2022-07-19 08:25:02 +1000

    messages/directories now show in a grid

--- src/components/Folder/Folder.tsx
+++ src/components/Folder/Folder.tsx
@@ -5,16 +5,14 @@ import directory from '../../types/directory.ts'
 
 interface FolderProps { folder: directory }
 // folder component, takes in directory and shows just the name of the current directory 
-const Folder: FC<FolderProps> = ({folder}) => (
+const Folder: FC<FolderProps> = ({ folder }) => (
 
   <div className={styles.Folder} data-testid="Folder">
     {
-    
+
       <div>
         <img className="img-fluid" src={folder_icon} />
-        <div>
         {folder.name}
-        </div>
       </div>
     }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App.js';
 import Appt from './dict_test.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 
