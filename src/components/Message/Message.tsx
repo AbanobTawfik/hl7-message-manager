325d644820c046b95033dfff49fbb7f65df83a87
Abanob Tawfik <goonpizza@hotmail.com>
2022-07-20 08:20:23 +1000

    added word wrap and new font. Will use redux to manage state

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -2,17 +2,14 @@ import React, { FC } from 'react';
 import styles from './Message.module.scss';
 import message from '../../types/message.ts'
 import message_icon from '../../resources/Icons/message.png'
+export function Message({ message }){
 
-
-interface MessageProps { message: message }
-
-const Message: FC<MessageProps> = ({ message }) => (
-  <div className={styles.Message} data-testid="Message">
+ return( <div className={styles.Message} data-testid="Message">
     <img className="img-fluid" src={message_icon} />
-      <div >{message.description}</div>
+      {message.description}
 
-      <div className='text'>{message.description}</div>
   </div>
 );
+}
 
 export default Message;
